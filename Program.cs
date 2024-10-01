2ce6106ca09bbce6aa05293671488ed214954208
ferreiraedilson <42985659+ferreiraedilson@users.noreply.github.com>
2024-10-01 09:19:27 -0300

    Ajuste nas mensagens de console

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
 ﻿using riskSystem.Reports;
 using riskSystemTest.Reports;
 
+
 var outPutQuestion01 = ReportOutPutFactory.GetFactoryReport<Question01Report>();
 outPutQuestion01.GenerateOutput();
+Console.WriteLine("Output of Question 01 was created");
+Console.WriteLine("");
 
 var outPutQuestion02 = ReportOutPutFactory.GetFactoryReport<Question02Report>();
 outPutQuestion02.GenerateOutput();
+Console.WriteLine("Output of Question 02 was created");
+Console.WriteLine("");
+
+Console.WriteLine("Press any key to close this window");
+Console.ReadKey();
+
